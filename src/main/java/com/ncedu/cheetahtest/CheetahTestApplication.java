becd83f53b6e17f4810a96ceea84ddcf03f1605b
SavStanis <svsodessa@gmail.com>
2020-11-12 20:24:37 +0200

    Init commit

---
+++ src/main/java/com/ncedu/cheetahtest/CheetahTestApplication.java
@@ -0,0 +1,13 @@
+package com.ncedu.cheetahtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheetahTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheetahTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ncedu/cheetahtest/CheetahtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncedu.cheetahtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheetahtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
