d57608c00fd3261805da4d7b1848d19e153ea539
SavStanis <svsodessa@gmail.com>
2020-11-14 14:27:25 +0200

    Added db creation script

---
+++ sql_ddl/db.sql
@@ -0,0 +1,20 @@
+create type developer_role as enum ('admin', 'manager', 'engineer');
+create type developer_status as enum ('active', 'inactive');
+
+create table developer (
+    id              serial primary key not null ,
+    email           varchar(100) not null ,
+    password        varchar(300) not null ,
+    name            varchar(100) not null ,
+    role            developer_role not null ,
+    status          developer_status not null ,
+    reset_token_id  integer
+);
+
+create table reset_token
+(
+    id           serial primary key,
+    token        varchar(50),
+    expiry_date  timestamp,
+    developer_id integer
+);
