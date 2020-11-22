c8a90ceb767e71c9a1ed76e50457a062dc3bfd3c
SavStanis <svsodessa@gmail.com>
2020-11-22 20:39:04 +0200

    Added missing constructors

--- src/main/java/com/ncedu/cheetahtest/entity/user/User.java
+++ src/main/java/com/ncedu/cheetahtest/entity/user/User.java
@@ -13,6 +13,19 @@ public class User {
     private String status;
     private int resetPassToken;
 
+    public User() {
+
+    }
+
+    public User(int id, String email, String pass, String name, String role, String status, int resetPassToken) {
+        this.id = id;
+        this.email = email;
+        this.pass = pass;
+        this.name = name;
+        this.role = role;
+        this.status = status;
+        this.resetPassToken = resetPassToken;
+    }
 
     public User(int id) {
         this.id = id;
