db8f51698d7c9a2f5886bc8ac7b4459fcb3aaf10
Kotyara <babanina.ev@gmail.com>
2020-11-23 18:14:53 +0200

    delete unnecessary verification in  UserServiceImpl

--- src/main/java/com/ncedu/cheetahtest/service/user/UserServiceImpl.java
+++ src/main/java/com/ncedu/cheetahtest/service/user/UserServiceImpl.java
@@ -6,7 +6,6 @@ import com.ncedu.cheetahtest.dao.user.UserDao;
 import com.ncedu.cheetahtest.entity.user.ResetToken;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -63,14 +62,12 @@ public class UserServiceImpl implements UserService {
 
   @Override
   @Transactional
-  @PreAuthorize("'admin'.equals(user.getRole())")
   public User editUser(User user) {
     return userDao.editUser(user);
   }
 
   @Override
   @Transactional
-  @PreAuthorize("'admin'.equals(user.getRole())")
   public User changeUserStatus(User user) {
     return userDao.changeUserStatus(user);
   }
