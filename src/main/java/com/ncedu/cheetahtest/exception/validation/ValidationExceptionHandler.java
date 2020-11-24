8b068b8ad487751ec202e67b4a9f1179f94d15cd
SavStanis <svsodessa@gmail.com>
2020-11-24 10:45:48 +0200

    API keys were moved from the travis config file to env variables

--- src/main/java/com/ncedu/cheetahtest/exception/validation/ValidationExceptionHandler.java
+++ src/main/java/com/ncedu/cheetahtest/exception/validation/ValidationExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -21,7 +20,6 @@ public class ValidationExceptionHandler extends ResponseEntityExceptionHandler {
                                                                   HttpHeaders headers,
                                                                   HttpStatus status, WebRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", new Date());
         body.put("status", status.value());
 
         Map<String, String> errors = ex.getBindingResult()
