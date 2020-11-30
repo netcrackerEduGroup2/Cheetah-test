487b0d402dee9d09dc466d06821bfa67b261df11
dima <dimarozhko2000@gmail.com>
2020-11-30 14:06:43 +0200

    Profiles pagination search deactivate

--- src/main/java/com/ncedu/cheetahtest/controller/userprofile/UserController.java
+++ src/main/java/com/ncedu/cheetahtest/controller/userprofile/UserController.java
@@ -53,6 +53,7 @@ public class UserController {
                                    @RequestParam("role") String role,
                                    @RequestParam("size") int size,
                                    @RequestParam("page") int page){
+
       return new UserPagination(userService.getSearchUserByNameEmailRole(name, email, role),
                                 size, page);
   }
