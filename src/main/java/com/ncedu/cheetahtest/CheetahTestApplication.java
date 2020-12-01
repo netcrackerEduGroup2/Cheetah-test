66ae92b0f874be8c8f314ed2ce6f535597714956
HellaxA <abereznikov64@gmail.com>
2020-12-02 00:08:03 +0200

    Deleted tests

--- src/main/java/com/ncedu/cheetahtest/CheetahTestApplication.java
+++ src/main/java/com/ncedu/cheetahtest/CheetahTestApplication.java
@@ -1,13 +1,7 @@
 package com.ncedu.cheetahtest;
 
-import com.ncedu.cheetahtest.dao.resettoken.ResetTokenDaoImpl;
-import com.ncedu.cheetahtest.dao.user.UserDaoImpl;
-import com.ncedu.cheetahtest.service.user.UserServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class CheetahTestApplication {
