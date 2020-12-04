24afbc41f56a286f9aec07c724637e814f05da5e
DMYTRO <GorovDimon11>
2020-12-04 18:28:25 +0200

    Adapted method

--- src/main/java/com/ncedu/cheetahtest/service/testscenario/TestScenarioServiceImpl.java
+++ src/main/java/com/ncedu/cheetahtest/service/testscenario/TestScenarioServiceImpl.java
@@ -1,22 +1,21 @@
 package com.ncedu.cheetahtest.service.testscenario;
 
+import com.ncedu.cheetahtest.dao.genericdao.AbstractActiveDao;
 import com.ncedu.cheetahtest.dao.testcase.TestCaseDao;
 import com.ncedu.cheetahtest.dao.testscenario.TestScenarioDao;
 import com.ncedu.cheetahtest.entity.testcase.TestCase;
 import com.ncedu.cheetahtest.entity.testscenario.*;
 import com.ncedu.cheetahtest.exception.testcase.TestCaseNotFoundException;
 import com.ncedu.cheetahtest.exception.testscenario.TestScenarioAlreadyExistsException;
-import com.ncedu.cheetahtest.exception.testscenario.TestScenarioNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TestScenarioServiceImpl implements TestScenarioService {
 
     private final TestScenarioDao testScenarioDao;
+    private final AbstractActiveDao<TestCase> testCaseGenDao;
     private final TestCaseDao testCaseDao;
 
     @Override
@@ -37,7 +36,7 @@ public class TestScenarioServiceImpl implements TestScenarioService {
                         testScenario.getTitle(),
                         testScenario.getId());
 
-        TestCase testCase = testCaseDao.findById(testScenario.getIdTestCase());
+        TestCase testCase = testCaseGenDao.findById(testScenario.getIdTestCase());
 
         if (testCase == null) {
             throw new TestCaseNotFoundException();
