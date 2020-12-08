8a5600f93e79d249bd81272bdf59756471126ed0
HellaxA <abereznikov64@gmail.com>
2020-12-08 22:58:33 +0200

    Code smells

--- src/main/java/com/ncedu/cheetahtest/controller/testcase/TestCaseController.java
+++ src/main/java/com/ncedu/cheetahtest/controller/testcase/TestCaseController.java
@@ -2,7 +2,7 @@ package com.ncedu.cheetahtest.controller.testcase;
 
 import com.ncedu.cheetahtest.entity.testcase.TestCase;
 import com.ncedu.cheetahtest.entity.testcase.TestCasePaginated;
-import com.ncedu.cheetahtest.service.testcase.TestCaseService;
+import com.ncedu.cheetahtest.service.testcase.crud.TestCaseService;
 import com.ncedu.cheetahtest.service.testcase.runwrapper.TestCaseLauncher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
