21b9c3008a0474ce8427e8e72f3f31bbf0fdf8cf
HellaxA <abereznikov64@gmail.com>
2020-12-22 17:40:09 +0200

    Fixed bugs

--- src/main/java/com/ncedu/cheetahtest/dao/testcase/TestCaseConsts.java
+++ src/main/java/com/ncedu/cheetahtest/dao/testcase/TestCaseConsts.java
@@ -94,12 +94,6 @@ public class TestCaseConsts {
         SELECT_ALL_PARAMS_FROM_TEST_CASE +
             "WHERE status = 'ACTIVE' AND title LIKE CONCAT ('%',?,'%')";
 
-    public static final String SET_EXECUTION_DATE_TO_NULL =
-            UPDATE +
-            "execution_cron_date = NULL, " +
-            "repeatable = NULL " +
-            WHERE_ID_EQUALS_Q;
-
     public static final String SET_EXECUTION_DATE_AND_REPEATABILITY =
             UPDATE +
             "execution_cron_date = ?, " +
