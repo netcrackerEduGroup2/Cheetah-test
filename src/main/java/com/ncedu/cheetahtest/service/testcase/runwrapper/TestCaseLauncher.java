e58a96ab8d5a61221e3f592aa115f7d89bd1d1fe
HellaxA <abereznikov64@gmail.com>
2020-12-28 01:38:02 +0200

    Fixed run

--- src/main/java/com/ncedu/cheetahtest/service/testcase/runwrapper/TestCaseLauncher.java
+++ src/main/java/com/ncedu/cheetahtest/service/testcase/runwrapper/TestCaseLauncher.java
@@ -1,5 +1,9 @@
 package com.ncedu.cheetahtest.service.testcase.runwrapper;
 
+import com.ncedu.cheetahtest.entity.generalentity.IdsDto;
+
 public interface TestCaseLauncher {
     void formActionForSelenium(int testCaseId);
+
+    void runTestCases(IdsDto idsDto);
 }
